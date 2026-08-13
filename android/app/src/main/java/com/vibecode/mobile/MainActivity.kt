91b3d66cade0b5f21cafa3da9ce6a908c5bb2797
Nguyen Nguyen <68273578+gutbai@users.noreply.github.com>
2026-08-13 17:01:52 +0700

    style(android): clean MainActivity Compose entry point

--- android/app/src/main/java/com/vibecode/mobile/MainActivity.kt
+++ android/app/src/main/java/com/vibecode/mobile/MainActivity.kt
@@ -6,6 +6,13 @@ import androidx.activity.compose.setContent
 import com.vibecode.mobile.ui.VibeCodeApp
 import com.vibecode.mobile.ui.VibeTheme
 
-class MainActivity:ComponentActivity(){
-    override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState);setContent{VibeTheme{VibeCodeApp()}}}
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VibeTheme {
+                VibeCodeApp()
+            }
+        }
+    }
 }
